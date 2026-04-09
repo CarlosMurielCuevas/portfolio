3470e7888f838aa5a3d797854f10c8b4c14395a8
Carlos <murielcaco2004@gmail.com>
2026-04-09 10:59:13 +0200

    feat: navbar y footer responsive con estilos globales

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrl: './navbar.scss'
 })
 export class NavbarComponent {
+  menuAbierto = signal(false);
+  scrolled = signal(false);
 
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled.set(window.scrollY > 50);
+  }
+
+  toggleMenu() {
+    this.menuAbierto.set(!this.menuAbierto());
+  }
+
+  cerrarMenu() {
+    this.menuAbierto.set(false);
+  }
 }
