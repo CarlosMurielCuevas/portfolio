ab1e5f8478a97199b8d01fd23abc2ed1590753b3
Carlos <murielcaco2004@gmail.com>
2026-04-09 11:39:47 +0200

    feat: sección sobre mí con datos personales y estilos responsive

--- src/app/features/sobre-mi/sobre-mi.ts
+++ src/app/features/sobre-mi/sobre-mi.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sobre-mi',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './sobre-mi.html',
-  styleUrl: './sobre-mi.scss',
+  styleUrl: './sobre-mi.scss'
 })
-export class SobreMiComponent {
-
-}
+export class SobreMiComponent {}
