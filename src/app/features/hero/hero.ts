afd9b1804a5d0fade7fdbd2288603aabe82d9d25
Carlos <murielcaco2004@gmail.com>
2026-04-09 11:36:28 +0200

    feat: sección hero con foto de perfil y estilos responsive

--- src/app/features/hero/hero.ts
+++ src/app/features/hero/hero.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './hero.html',
-  styleUrl: './hero.scss',
+  styleUrl: './hero.scss'
 })
-export class HeroComponent {
-
-}
+export class HeroComponent {}
