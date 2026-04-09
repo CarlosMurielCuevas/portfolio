53ed51d69e62629047f2b830ea595576e7af1a0f
Carlos <murielcaco2004@gmail.com>
2026-04-09 10:18:43 +0200

    feat: estructura base de componentes y rutas con lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/hero/hero').then((m) => m.HeroComponent),
+  },
+  {
+    path: 'sobre-mi',
+    loadComponent: () =>
+      import('./features/sobre-mi/sobre-mi').then((m) => m.SobreMiComponent),
+  },
+  {
+    path: 'experiencia',
+    loadComponent: () =>
+      import('./features/experiencia/experiencia').then((m) => m.ExperienciaComponent),
+  },
+  {
+    path: 'habilidades',
+    loadComponent: () =>
+      import('./features/habilidades/habilidades').then((m) => m.HabilidadesComponent),
+  },
+  {
+    path: 'educacion',
+    loadComponent: () =>
+      import('./features/educacion/educacion').then((m) => m.EducacionComponent),
+  },
+  {
+    path: 'proyectos',
+    loadComponent: () =>
+      import('./features/proyectos/proyectos').then((m) => m.ProyectosComponent),
+  },
+  {
+    path: 'contacto',
+    loadComponent: () =>
+      import('./features/contacto/contacto').then((m) => m.ContactoComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/app/features/educacion/educacion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-educacion',
+  imports: [],
+  templateUrl: './educacion.html',
+  styleUrl: './educacion.scss',
+})
+export class EducacionComponent {
+
+}
